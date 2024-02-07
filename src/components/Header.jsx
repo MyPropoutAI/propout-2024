345c9dd1ae6d06a0c89072fa4b60f01b6a268ee5
1am-programmer <dannywebexpertise@gmail.com>
2024-02-07 14:44:11 -0800

    added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
 const Header = () => {
-  return <div className="bg-dark-blue">esfwsvd</div>;
+  return (
+    <div className="flex justify-between items-center bg-purple-900 py-3 text-white">
+      <img
+        className="ml-10"
+        src="src/components/img/pro2 1.svg"
+        alt="Prop Logo"
+      />
+
+      <div className="flex items-center">
+        <img src="src/components/img/Ellipse 10.svg" alt="Eclipse" />
+        <Button className="mx-5">Explore Propout</Button>
+        <Button className="mr-20">Connect Wallet</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
