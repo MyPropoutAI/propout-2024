eefb83cf4b195a43f7aff33e4e4e72a5f35ee0fe
highfee <odediranifeoluwa7@gmail.com>
2024-02-22 11:53:22 +0100

    add color to back btn

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,11 +1,11 @@
 import { BiArrowBack } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
-const Back = () => {
+const Back = ({ color }) => {
   const navigate = useNavigate();
   return (
     <div className=" cursor-pointer">
-      <BiArrowBack size={25} onClick={() => navigate(-1)} />
+      <BiArrowBack size={25} onClick={() => navigate(-1)} className={color} />
     </div>
   );
 };
