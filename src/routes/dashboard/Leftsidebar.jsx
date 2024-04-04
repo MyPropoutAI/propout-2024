284ed83f55823efd1f01d3208e27a60bb89bd58e
1am-programmer <dannywebexpertise@gmail.com>
2024-04-04 14:02:13 -0800

    colored nav-bar

--- src/routes/dashboard/Leftsidebar.jsx
+++ src/routes/dashboard/Leftsidebar.jsx
@@ -1,46 +1,90 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Button } from "../../components/ui/button";
+import { Link, useLocation } from "react-router-dom";
+
+const links = [
+  {
+    name: "Overview",
+    path: "/dashboard/overview",
+    img: "/images/overview.svg",
+  },
+  {
+    name: " List Property",
+    path: "/dashboard/list",
+    img: "/images/List-p.svg",
+  },
+  {
+    name: " My Property",
+    path: "/dashboard/properties",
+    img: " /images/my-p.svg",
+  },
+  {
+    name: " Swap",
+    path: "/testnet/swap",
+    img: "/images/swap.svg",
+  },
+  {
+    name: " Stake ",
+    path: "/testnet/stake",
+    img: "/images/stake.svg",
+  },
+  {
+    name: "Task",
+    path: "/dashboard/task",
+    img: "/images/task.svg",
+  },
+  {
+    name: " Refer a friend",
+    path: "/dashboard/referral",
+    img: "/images/refer.svg",
+  },
+  {
+    name: "Settings",
+    path: "/dashboard/setting",
+    img: "/images/setting.svg",
+  },
+];
 
 const Leftsidebar = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
+  const activeLink =
+    "bg-gradient-to-r from-[#964DC3]  to-[#F42A8B] text-white ";
+
   return (
-    <div className="flex flex-col justify-between gap-10 bg-white p-4 text-[12px] rounded-md font-semibold text-[#320051]">
+    <div className="flex flex-col justify-between gap-10 bg-white p-4 text-[12px] rounded-md font-semibold text-[#320051] w-[200px]">
       <div className="flex flex-col gap-2">
-        <Link to="/dashboard/overview" className="flex items-center gap-1">
-          <img src="/images/overview.svg" alt="icon" />
-          Overview
-        </Link>
-        <Link to="/dashboard/list" className="flex items-center gap-1">
-          <img src="/images/List-p.svg" alt="icon" />
-          List Property
-        </Link>
-        <Link to="/dashboard/properties" className="flex items-center gap-1">
-          <img src="/images/my-p.svg" alt="icon" />
-          My Property
-        </Link>
-        <Link to="/testnet/swap" className="flex items-center gap-1">
-          <img src="/images/swap.svg" alt="icon" />
-          Swap
-        </Link>
-        <Link to="/testnet/stake" className="flex items-center gap-1">
-          <img src="/images/stake.svg" alt="icon" />
-          Stake
-        </Link>
-        <Link to="/dashboard/task" className="flex items-center gap-1">
-          <img src="/images/task.svg" alt="icon" />
-          Task
-        </Link>
-        <Link to="/dashboard/referral" className="flex items-center gap-1">
-          <img src="/images/refer.svg" alt="icon" />
-          Refer a friend
-        </Link>
-        <Link to="/dashboard/setting" className="flex items-center gap-1">
-          <img src="/images/setting.svg" alt="icon" />
-          Settings
-        </Link>
+        {links.map((item) => (
+          <Link to={item.path} className="flex items-center gap-1">
+            <div
+              className={`${
+                pathname == item.path && "bg-[#964DC3]"
+              } w-7 rounded-lg`}
+            >
+              <img
+                src={item.img}
+                alt="icon"
+                className={`${
+                  pathname == item.path && "invert brightness-50 contrast-200"
+                }`}
+              />
+            </div>
+            <span
+              className={`${
+                pathname == item.path && activeLink
+              } flex-1 h-full flex items-center px-2 rounded-sm`}
+            >
+              {item.name}
+            </span>
+          </Link>
+        ))}
       </div>
       <div>
-        <Button size="guide" className="text-white px-16 py-2 rounded-md">
+        <Button
+          size="guide"
+          className="text-white px-16 py-2 rounded-md w-full"
+        >
           <Link to="">GUIDE</Link>
         </Button>
       </div>
