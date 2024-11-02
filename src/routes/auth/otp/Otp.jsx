7ddc7f31a7eff5bb03942082fd6f1e74257821af
laondev5 <ekohabraham5@gmail.com>
2024-11-02 05:21:08 +0100

    vercel reload error fixed1

--- src/routes/auth/otp/Otp.jsx
+++ src/routes/auth/otp/Otp.jsx
@@ -14,6 +14,7 @@ import { useSelector } from "react-redux";
 import jwt from "jsonwebtoken";
 //import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../../../components/ui/button";
+// import {useRoute}
 
 const Otp = () => {
   const [isLoading, setIsLoading] = useState(false);
