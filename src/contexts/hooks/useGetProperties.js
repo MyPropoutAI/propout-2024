d2857bf27e12d90a88962a453388e88bd7b37f50
laondev5 <ekohabraham5@gmail.com>
2024-11-02 20:58:38 +0100

    dfdc1

--- src/contexts/hooks/useGetProperties.js
+++ src/contexts/hooks/useGetProperties.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export const getProperties = async () => {
-  const res = await fetch("https://proput-db-g4te.onrender.com/all");
+  const res = await fetch("https://proput-db-g4t.onrender.com/all");
   const data = await res.json();
   console.log(data);
   return data;
